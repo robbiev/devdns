9821bee002c5e9c79fe1f8500f20da5d169aa205
Dan Sosedoff <dan.sosedoff@gmail.com>
2014-12-28 15:07:06 -0600

    Add -ip flag to specify resolve ip address

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ import (
 	"github.com/miekg/dns"
 )
 
+var resolveIp net.IP
+
 func handleRequest(w dns.ResponseWriter, r *dns.Msg) {
 	q := r.Question[0]
 
@@ -19,7 +21,7 @@ func handleRequest(w dns.ResponseWriter, r *dns.Msg) {
 		m.SetReply(r)
 		a := new(dns.A)
 		a.Hdr = dns.RR_Header{Name: q.Name, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 600}
-		a.A = net.IPv4(127, 0, 0, 1)
+		a.A = resolveIp
 		m.Answer = []dns.RR{a}
 		w.WriteMsg(m)
 		log.Printf("%s (RESOLVED)\n", info)
@@ -34,10 +36,18 @@ func handleRequest(w dns.ResponseWriter, r *dns.Msg) {
 
 func main() {
 	var addr = flag.String("addr", "127.0.0.1:5300", "listen address")
+	var ip = flag.String("ip", "127.0.0.1", "resolve ip address")
+
 	flag.Parse()
+
+	resolveIp = net.ParseIP(*ip)
+	if resolveIp == nil {
+		log.Fatalf("Invalid ip address: %s\n", *ip)
+	}
 
 	server := &dns.Server{Addr: *addr, Net: "udp"}
 	server.Handler = dns.HandlerFunc(handleRequest)
-	log.Printf("Listening on %s\n", *addr)
+
+	log.Printf("Listening on %s, resolving to %s\n", *addr, *ip)
 	log.Fatal(server.ListenAndServe())
 }
